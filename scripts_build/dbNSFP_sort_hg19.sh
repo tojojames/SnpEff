cf15230f125e610bef075b81c99f0cf70f481db1
Tojo James <tjcstockholm@gmail.com>
2017-11-27 14:44:22 +0100

    build with lower memory usage
    ### For a low memory usage.Previous script failed inspite of providing 100 GB RAM. Following script might consume more time for I/O but consumes less memory.
    
    #!/bin/sh
    version="3.2a"
    touch dbNSFP${version}_hg19.txt 
    for file in dbNSFP${version}_variant.chr*; do cat $file|/home/tojjam/exomeSeq/snpEff/scripts/dbNSFP_sort.pl 7 8 >>dbNSFP${version}_hg19.txt; done

--- scripts_build/dbNSFP_sort_hg19.sh
+++ scripts_build/dbNSFP_sort_hg19.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+"""#!/bin/sh
 
 version="3.2a"
 
@@ -6,3 +6,10 @@ cat dbNSFP${version}_variant.chr* \
 	| $HOME/snpEff/scripts_build/dbNSFP_sort.pl 7 8 \
 	> dbNSFP${version}_hg19.txt
 
+"""
+### Forloop script written for a low memory usage.Above script failed inspite of providing 100 GB RAM. Following script might consume more time for I/O but consumes less memory.
+
+#!/bin/sh
+version="3.2a"
+touch dbNSFP${version}_hg19.txt 
+for file in dbNSFP${version}_variant.chr*; do cat $file|/home/tojjam/exomeSeq/snpEff/scripts/dbNSFP_sort.pl 7 8 >>dbNSFP${version}_hg19.txt; done
